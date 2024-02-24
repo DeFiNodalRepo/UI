148febb33f26acc0786e678c76af7e7c459999db
Cucuy555 <cucuycy555@gmail.com>
2024-02-24 16:20:56 +0200

    comments on todo logo

--- src/components/Sidebar/Sidebard.tsx
+++ src/components/Sidebar/Sidebard.tsx
@@ -72,7 +72,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        {/* <NavLink to="/">
+        {/*
+        Logo should be here, Image preferably
+        <NavLink to="/">
           <img src={Logo} alt="Logo" />
         </NavLink> */}
 
