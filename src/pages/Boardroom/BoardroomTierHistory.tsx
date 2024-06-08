a2d784fe487c3770f7a199e7366933def7fa3f36
Cucuy555 <cucuycy555@gmail.com>
2024-06-08 11:21:15 +0300

    fixed rendering histories bug

--- src/pages/Boardroom/BoardroomTierHistory.tsx
+++ src/pages/Boardroom/BoardroomTierHistory.tsx
@@ -203,51 +203,53 @@ function BoardroomTierHistory({ tierIndex }) {
     return acc
   }, {})
 
+  const tierHistoryView = groupedByTierId[tierIndex].map((histories, index) => {
+    return histories
+  })
+
+  console.log(tierHistoryView)
+
   return (
     <ul role="list" className="divide-gray-100 divide-y">
-      {Object.entries(groupedByTierId[tierIndex]).map((histories) => (
-        <React.Fragment key={histories.tierId}>
-          {histories.map((history, index) => (
-            <li
-              key={index}
-              className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap"
-            >
-              <div>
-                <p className="text-gray-900 text-sm font-semibold leading-6">
-                  {history.dateStacked}
-                </p>
-                <div className="text-gray-500 mt-1 flex items-center gap-x-2 text-xs leading-5">
-                  <p>{history.amountStacked}</p>
-                  <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
-                    <circle cx={1} cy={1} r={1} />
-                  </svg>
-                  <p>
-                    expiryDate
-                    {history.expiryDate}
-                  </p>
-                </div>
-              </div>
-              <dl className="flex w-full flex-none justify-between gap-x-8 sm:w-auto">
-                <div className="flex -space-x-0.5">
-                  <dt className="sr-only">Timeleft</dt>
-                  {history.timeLeft}
-                </div>
-                <div className="flex gap-x-2.5">
-                  <dt>
-                    <span className="sr-only">tier id</span>
-                    {history.tierId}
-                  </dt>
-                  <dd className="text-gray-900 text-sm leading-6">
-                    expiryDate: {history.expiryDate}
-                  </dd>
-                </div>
-              </dl>
+      {groupedByTierId[tierIndex].map((history, index) => (
+        <li
+          key={index}
+          className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap"
+        >
+          <div>
+            <p className="text-gray-900 text-sm font-semibold leading-6">
+              {history.dateStacked}
+            </p>
+            <div className="text-gray-500 mt-1 flex items-center gap-x-2 text-xs leading-5">
+              <p>{history.amountStacked}</p>
+              <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
+                <circle cx={1} cy={1} r={1} />
+              </svg>
+              <p>
+                expiryDate
+                {history.expiryDate}
+              </p>
+            </div>
+          </div>
+          <dl className="flex w-full flex-none justify-between gap-x-8 sm:w-auto">
+            <div className="flex -space-x-0.5">
+              <dt className="sr-only">Timeleft</dt>
+              {history.timeLeft}
+            </div>
+            <div className="flex gap-x-2.5">
+              <dt>
+                <span className="sr-only">tier id</span>
+                {history.tierId}
+              </dt>
+              <dd className="text-gray-900 text-sm leading-6">
+                expiryDate: {history.expiryDate}
+              </dd>
+            </div>
+          </dl>
 
-              <input />
-              <button>Withdraw</button>
-            </li>
-          ))}
-        </React.Fragment>
+          <input />
+          <button>Withdraw</button>
+        </li>
       ))}
     </ul>
   )
